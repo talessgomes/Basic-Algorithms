cbe8479a85a8a1df577290d846af9e84e2046b78
unknown <tales.santos.gomes@outlook.com>
2022-11-25 10:28:51 -0300

    finalizado-exercicio-09

---
+++ ex09/index.js
@@ -0,0 +1,12 @@
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ['learning', ...fragment, 'is', 'fun']; // Change this line
+    return sentence;
+}
+
+console.log(spreadOut());
+
+
+/*Neste desafio eu vou fazer uma cópia do array, usando spread,
+também vou adicionar alguns valores no array. Que será implementado
+na variável sentence, que será o retorno a minha função. */
