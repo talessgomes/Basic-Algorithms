7b6bce6345b08e1ad5938348677095d31662ce76
Tales Gomes <tales.santos.gomes@outlook.com>
2022-11-30 10:46:01 -0300

    finalizado-exercicio-17

---
+++ ex17/index.js
@@ -0,0 +1,46 @@
+let users = {
+    Alan: {
+        age: 27,
+        online: true
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: true
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function isEveryoneHere(userObj) {
+    // Only change code below this line
+
+    if (userObj.hasOwnProperty('Alan') &&
+        userObj.hasOwnProperty('Jeff') &&
+        userObj.hasOwnProperty('Sarah') &&
+        userObj.hasOwnProperty('Ryan')) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+    // Only change code above this line
+}
+
+console.log(isEveryoneHere(users));
+
+
+/* exemplos
+ 
+users.hasOwnProperty('Alan');
+'Alan' in users;
+ 
+ 
+estes dois exemplos estão certos
+ */
