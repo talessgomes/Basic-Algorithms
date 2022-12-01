cce21dbc1e56634c36d4ec9eacd413f4c0277863
Tales Gomes <tales.santos.gomes@outlook.com>
2022-12-01 09:32:57 -0300

    finalizado-exercicio-19

---
+++ ex19/index.js
@@ -0,0 +1,29 @@
+let users = {
+    Alan: {
+        age: 27,
+        online: false
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: false
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function getArrayOfUsers(obj) {
+    // Only change code below this line
+    return Object.keys(obj);
+    // Only change code above this line
+}
+
+console.log(getArrayOfUsers(users));
+
+/*Para ter o retorno do array, eu preciso digitar Object.keys para dizer
+que quero a chave do objecto e preciso passar o parâmetro dentro dos parênteses */
